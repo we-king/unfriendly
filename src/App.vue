3416f74547e844965f9a6fa571fa77127f69ec1d
we-king <95weking@gmail.com>
2024-07-28 17:25:39 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-import {  RouterView } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
 </script>
 
 <template>
-  <RouterView />
+  <HomeView />
 </template>
 
 <style scoped>
